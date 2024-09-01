251c62318f780102e664ce4ec547f5731e180fa5
Lorhan Sohaky <contato+gi74@lorhan.me>
2024-09-01 10:59:04 -0300

    style: fix code style

--- examples/signer.js
+++ examples/signer.js
@@ -1,9 +1,8 @@
 import {Signer} from '../dist/index.js';
 
-const signer = new Signer({secretKey: 'secret-key'})
-const signature = await signer.sign('foo')
+const signer = new Signer({secretKey: 'secret-key'});
+const signature = await signer.sign('foo');
 
-const data = await signer.unsign(signature, 5)
-const original = new TextDecoder().decode(data)
+const data = await signer.unsign(signature, 5);
+const original = new TextDecoder().decode(data);
 console.log(original); // foo
-
--- examples/url-safe-timed-serialization.js
+++ examples/url-safe-timed-serialization.js
@@ -2,12 +2,12 @@ import {URLSafeTimedSerializer} from '../dist/index.js';
 const authSerializer = new URLSafeTimedSerializer({secretKey: 'secret key', salt: 'auth'});
 const token = await authSerializer.stringify({id: 5, name: 'itsdangerous'});
 
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-try{
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+try {
 	await sleep(6000);
 	// note: the following line will throw an error if the token is expired
 	const data = await authSerializer.parse(token, undefined, 5);
-}catch(err){
+} catch (err) {
 	console.log(err.name); // SignatureExpiredError
 	console.log(err.message); // Signature age 6 > 5 seconds
 }
